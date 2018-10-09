efa8e122f1f80d7805e7a308548c6ed7b2becb73
Dan Van Brunt <dvanbrunt@gmail.com>
2018-10-09 10:34:14 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          Stackname: {process.env.ReactAppStackname}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
