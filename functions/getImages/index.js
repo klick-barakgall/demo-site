8efbbaadbc8a0b2360c5511a3a51ffd1ae7faef1
Dan Van Brunt <dvanbrunt@gmail.com>
2018-10-09 15:31:10 -0400

    WIP

--- functions/getImages/index.js
+++ functions/getImages/index.js
@@ -1,8 +1,20 @@
 
 exports.handler = (event, context, callback) => {
   console.log('event JSON', JSON.stringify(event));
-  
+  const responseBody = {
+    images: [
+      {url: 'https://loremflickr.com/320/240/dog?lock=3'},
+      {url: 'https://loremflickr.com/320/240/dog?lock=4'},
+      {url: 'https://loremflickr.com/320/240/dog?lock=5'}
+    ]
+  };
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(responseBody)
+  };
 
-
-  callback(null, {});
+  callback(null, response);
 };
